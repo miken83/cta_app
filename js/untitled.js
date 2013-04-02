b839f1dbfaf2db4ef208c8a8aa03ba04dfafbcc2
Michael Nelson <miken83@gmail.com>
2013-04-02 18:40:08 -0500

    added background image, added bus routes

--- js/untitled.js
+++ js/untitled.js
@@ -12,9 +12,19 @@ function getAlertStatus(){
 		test.open("GET", $URL, false);
 		test.send();
 		xmldoc = test.responseXML;
-		$('.status').html("<h1>" + xmldoc.getElementsByTagName("RouteInfo")[0].childNodes['0'].textContent + "</h1><h3>" + xmldoc.getElementsByTagName("RouteStatus")[0].childNodes['0'].textContent + "</h3>");
+		$('.status').html("<div class='status'><h1>" + xmldoc.getElementsByTagName("RouteInfo")[0].childNodes['0'].textContent + "</h1><h3>" + xmldoc.getElementsByTagName("RouteStatus")[0].childNodes['0'].textContent + "</h3></div>");
 	});
-
+	$('.bus').click(function(){
+		$('.route').removeClass("active");
+		$(".collapse").collapse('toggle');		
+		$routeId = $(this).data("value");
+		var $URL = $baseUrl + $routeId;
+		var test = new XMLHttpRequest();
+		test.open("GET", $URL, false);
+		test.send();
+		xmldoc = test.responseXML;
+		$('.status').html("<div class='status'><h1>" + xmldoc.getElementsByTagName("RouteInfo")[0].childNodes['0'].textContent + "</h1><h3>" + xmldoc.getElementsByTagName("RouteStatus")[0].childNodes['0'].textContent + "</h3></div>");
+	});
  
 
 
