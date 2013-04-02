896e31f091f3e29905659ec7236e1d163f0c44d0
Michael Nelson <miken83@gmail.com>
2013-04-01 21:54:22 -0500

    significantly improved javascript

---
+++ js/untitled.js
@@ -0,0 +1,21 @@
+function getAlertStatus(){
+	var $routeId;
+	var $baseUrl = 'http://www.transitchicago.com/api/1.0/routes.aspx?routeid=';
+	var $URL;
+	$('.route').click(function(){
+		$('.route').removeClass("active");
+		$(".collapse").collapse('toggle');
+		$routeId = $(this).data("value");
+		$(this).toggleClass("active");
+		var $URL = $baseUrl + $routeId;
+		var test = new XMLHttpRequest();
+		test.open("GET", $URL, false);
+		test.send();
+		xmldoc = test.responseXML;
+		$('.status').html("<h1>" + xmldoc.getElementsByTagName("RouteInfo")[0].childNodes['0'].textContent + "</h1><h3>" + xmldoc.getElementsByTagName("RouteStatus")[0].childNodes['0'].textContent + "</h3>");
+	});
+
+ 
+
+
+};
